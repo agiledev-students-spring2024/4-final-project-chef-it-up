20c7be49c57d3971ddde07f93c342acc2fe0ae3f
Walid Al-Eisawi <walidal-eisawi@10-16-14-77.dynapool.wireless.nyu.edu>
2024-03-04 01:38:45 -0500

    Added about us page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,7 @@ import MyRecipes from './MyRecipes'
 import GenerateRecipe from "./GenerateRecipe"
 import Profile from './Profile'
 import Header from './Header'
+import AboutUs from './AboutUs'
 
 import './App.css';
 
@@ -23,6 +24,7 @@ function App() {
           <Route  path="/addRecipe" element={<AddRecipe />} />
           <Route  path="/generateRecipe" element={<GenerateRecipe />} />
           <Route  path="/myProfile" element={<Profile />} />
+          <Route path="/aboutUs" element={<AboutUs />} />
           
         </Routes>
       </Router>
