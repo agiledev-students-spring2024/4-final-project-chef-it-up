dfdf7f997d899075c31acdf5e9f56855b1701527
Walid Al-Eisawi <walidal-eisawi@10-16-14-77.dynapool.wireless.nyu.edu>
2024-03-04 01:41:48 -0500

    completed About us

---
+++ front-end/src/AboutUs.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./AboutUs.css"
+
+const AboutUs = () =>{
+    return (
+        <div className="about-us-container">
+            <div className="about-us-content">
+                <h1>About Us</h1>
+                <p>
+                    Welcome to Chef It Up! We are passionate about making cooking an enjoyable and delicious experience for everyone.
+                </p>
+                <p>
+                    Chef it Up is an app that will not only help you reduce your food waste, but also give you plenty more options.
+                </p>
+                <p>
+                    Whether you're a student looking for simple recipes, a stay-at-home mom trying to keep track of what ingredients you have, or anyone else, we have something for you!
+                </p>
+                <p>
+                    Happy cooking!
+                </p>
+            </div>
+        </div>
+    )
+};
+
+export default AboutUs;
