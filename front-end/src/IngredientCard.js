9d09fc2f2bd33d5fd2ae83012f9f0e6bc353f0b7
walidaleis <wa2076@nyu.edu>
2024-04-13 17:05:11 -0400

    implemented adding ingredient to database

--- front-end/src/IngredientCard.js
+++ front-end/src/IngredientCard.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './IngredientCard.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./IngredientCard.css";
 
 const IngredientCard = ({ ingredient, baseUrl }) => {
-    
-    const imgSrc = `https://picsum.photos/200?id=${ingredient.id}`;
+  const imgSrc = `http://localhost:3001/${ingredient.img}`;
 
-    return (
-        <div className="ingredient-card">
-        <Link className="ingredient-link" to={`${baseUrl}/${ingredient.id}`}>
-            
-                <h1 className='ingredient-title'>{ingredient.ingredient_name}</h1>
-                <img className='ingredient-card-image' src={imgSrc} alt={`${ingredient.name}`} />            
-        </Link>
-        </div>
-    );
+  return (
+    <div className="ingredient-card">
+      <Link className="ingredient-link" to={`${baseUrl}/${ingredient._id}`}>
+        <h1 className="ingredient-title">{ingredient.ingredient_name}</h1>
+        <img
+          className="ingredient-card-image"
+          src={imgSrc}
+          alt={`${ingredient.name}`}
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default IngredientCard;
